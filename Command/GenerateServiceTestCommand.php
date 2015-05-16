fccf8a593e05bbb52962a40be2697ee21391e47e
dtonder <delf.tonder@gmx.de>
2015-05-16 18:47:11 +0200

    inital commit

---
+++ Command/GenerateServiceTestCommand.php
@@ -0,0 +1,85 @@
+<?php
+namespace Selenior\UtilBundle\Command;
+
+use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateServiceTestCommand extends ContainerAwareCommand
+{
+
+    /**
+     * @var TwigEngine
+     */
+    private $templating;
+
+    public function configure()
+    {
+        $this
+            ->setName('selenior:util:generate-service-test')
+            ->addArgument('class', InputArgument::REQUIRED, 'fq class name of service')
+            ->setDescription('Generates a basic php-unit file with mocked services');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->templating = $this->getContainer()->get('templating');
+
+        $className = $input->getArgument('class');
+        if (!class_exists($className)) {
+            throw new \Exception('class not found');
+        }
+
+        $reflectionClass = new \ReflectionClass($className);
+        $testNamespace = $this->getTestNamespace($output, $reflectionClass);
+
+
+        $parameters = $reflectionClass->getConstructor()->getParameters();
+        $useStatments = [];
+        $mocks = [];
+        foreach($parameters as $parameter) {
+            $parameterClass = $parameter->getClass();
+            $output->writeln('checking parameter ' . $parameterClass->getName());
+            $memberName = lcfirst($parameterClass->getShortName());
+            $mocks[] = [
+                'mocked_class_name' => $parameterClass->getName(),
+                'member_name' => $memberName,
+            ];
+        }
+        $serviceMemberName = lcfirst($reflectionClass->getShortName());
+        $generatedCode = $this->templating->render(
+            'SeleniorUtilBundle::phpunit.template.php.twig',
+            [
+                'test_namespace' => $testNamespace,
+                'use_statements' => $useStatments,
+                'original_short_name' => $reflectionClass->getShortName(),
+                'original_full_name' => $reflectionClass->getName(),
+                'service_member_name' => $serviceMemberName,
+                'mocks' => $mocks
+            ]
+        );
+        $output->writeln($generatedCode);
+
+        throw new \LogicException('You must override the execute() method in the concrete command class.');
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param \ReflectionClass $reflectionClass
+     * @return mixed|string
+     */
+    protected function getTestNamespace(OutputInterface $output, \ReflectionClass $reflectionClass)
+    {
+        $namespaceName = $reflectionClass->getNamespaceName();
+        if (strpos($namespaceName, 'Bundle')) {
+            $testNamespace = str_replace('Bundle', 'Bundle\Tests', $namespaceName);
+        } else {
+            $output->writeln('couldnt find "Bundle" in original class namespace');
+            $testNamespace = $namespaceName . '\Tests';
+        }
+        $output->writeln('namespace for generated Test: ' . $testNamespace);
+        return $testNamespace;
+    }
+}
---
+++ UtilBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace tps\UtilBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UtilBundle extends Bundle
+{
+}
